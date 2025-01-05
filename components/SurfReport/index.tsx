3a38f3dcd9b97473b438d49a6ca9f4be98d65dad
Taylor Ho <taylorkmho@gmail.com>
2025-01-04 22:33:11 -0800

    simplifying ui

--- components/SurfReport/index.tsx
+++ components/SurfReport/index.tsx
@@ -29,59 +29,58 @@ export default function SurfReport({ island = "oahu" }: { island?: string }) {
       {report.discussion && report.discussion.length > 0 && (
         <Card>
           <CardContent className="pt-6 space-y-4">
-            <p className="text-4xl font-bold">
+            <p className="text-2xl font-bold">
               {new Date(report.lastBuildDate).toLocaleString("en-US", {
                 month: "long",
                 day: "numeric",
                 year: "numeric",
               })}
             </p>
-            <div className="space-y-2 text-base font-mono">
+            <div className="space-y-2 text-sm text-secondary-foreground font-normal font-mono">
               {report.discussion.map((paragraph, index) => (
                 <p key={index}>{decode(paragraph)}</p>
               ))}
             </div>
-            <div className="grid gap-4 grid-cols-2 md:grid-cols-4">
+            <div className="flex flex-wrap gap-4">
               {report.waveHeights.map((wave, index) => (
                 <div
                   key={index}
-                  className="text-center rounded-lg border p-4 text-card-foreground"
+                  className="flex flex-col items-center gap-1 text-center text-card-foreground w-16"
                 >
-                  <h6 className="font-semibold text-muted-foreground text-sm">
+                  <h6 className="font-semibold text-muted-foreground text-xs">
                     {wave.direction}
                   </h6>
-                  <div className="flex flex-col items-center justify-center gap-2 mt-2 text-2xl font-bold">
-                    <span>{wave.height} ft</span>
-                    <span
-                      className={cn(
-                        "flex items-center gap-1.5 text-xs rounded-full px-2 h-5",
-                        wave.trend === "increasing" &&
-                          "text-green-500 bg-green-500/10",
-                        wave.trend === "decreasing" &&
-                          "text-red-500 bg-red-500/10",
-                        wave.trend === "steady" &&
-                          "text-muted-foreground bg-muted-foreground/10"
-                      )}
-                    >
-                      {wave.trend &&
-                        (wave.trend === "increasing" ? (
-                          <>
-                            <FaArrowTrendUp className="size-3" />
-                            Increasing
-                          </>
-                        ) : wave.trend === "decreasing" ? (
-                          <>
-                            <FaArrowTrendDown className="size-3" />
-                            Decreasing
-                          </>
-                        ) : (
-                          <>
-                            <FaArrowRightLong className="size-3" />
-                            Steady
-                          </>
-                        ))}
-                    </span>
-                  </div>
+
+                  <span className="text-base text-secondary-foreground font-bold">
+                    {wave.height} ft
+                  </span>
+                  <span
+                    className={cn(
+                      "flex items-center gap-1.5 text-xs rounded-full px-2 h-5",
+                      wave.trend === "increasing" &&
+                        "text-green-500 bg-green-500/10",
+                      wave.trend === "decreasing" &&
+                        "text-red-500 bg-red-500/10",
+                      wave.trend === "steady" &&
+                        "text-muted-foreground bg-muted-foreground/10"
+                    )}
+                    title={
+                      wave.trend === "increasing"
+                        ? "Increasing"
+                        : wave.trend === "decreasing"
+                        ? "Decreasing"
+                        : "Steady"
+                    }
+                  >
+                    {wave.trend &&
+                      (wave.trend === "increasing" ? (
+                        <FaArrowTrendUp className="size-3" />
+                      ) : wave.trend === "decreasing" ? (
+                        <FaArrowTrendDown className="size-3" />
+                      ) : (
+                        <FaArrowRightLong className="size-3" />
+                      ))}
+                  </span>
                 </div>
               ))}
             </div>
