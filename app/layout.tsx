ef7d4397fc22cb4992ee174ca903b531588cef0e
Taylor Ho <taylorkmho@gmail.com>
2025-01-15 23:31:14 -0800

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-touch-icon.png"
+          sizes="180x180"
+        />
+        <link rel="icon" href="/favicon-32x32.png" sizes="32x32" />
+        <link rel="icon" href="/favicon-16x16.png" sizes="16x16" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body
         className={`${fontSans.variable} ${fontSerif.variable} ${fontPixel.variable} ${fontSans.className} dark antialiased`}
       >
