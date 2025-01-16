560c5d6e66b68198353521dc4170679cae90ea98
Taylor Ho <taylorkmho@gmail.com>
2025-01-15 22:01:19 -0800

    refactor animation

--- components/AnimatedPuns.tsx
+++ components/AnimatedPuns.tsx
@@ -1,53 +1,67 @@
-import { motion, useAnimate } from "framer-motion";
-import { useEffect } from "react";
+import { motion } from "framer-motion";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 
 const puns = [
   "Sea the future",
   "Wave to tomorrow",
   "Deep learning, deeper water",
   "Large wave model",
-  "SURF-GPT (GNARLY PREDICTION TRANSFORMER)",
+  "SURF-GPT (GNARLY PREDICTION",
+  "TRANSFORMER)",
   "THE ORACLE OF OCEANSIDE",
   "PROPHECY WITH A PORPOISE",
   "FINE-TUNING THE PIPELINE",
   "THE WAVE WHISPERER",
   "CTRL+SEA",
+  "SEAMANTIC SEARCH",
 ];
 
+const SLIDE_INTERVAL = 2000;
+
 export default function AnimatedPuns() {
-  const [scope, animate] = useAnimate();
+  const measureRef = useRef<HTMLSpanElement>(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [height, setHeight] = useState(0);
+
+  useLayoutEffect(() => {
+    if (!measureRef.current) return;
+    setHeight(measureRef.current.offsetHeight);
+  }, [measureRef]);
 
   useEffect(() => {
-    const animateText = async () => {
-      let currentIndex = 0;
-      while (true) {
-        await animate(
-          scope.current,
-          { y: -currentIndex * 20 },
-          { duration: 2, ease: "anticipate" }
-        );
-        currentIndex += 2;
-        await new Promise((resolve) => setTimeout(resolve, 5000));
-      }
-    };
-
-    animateText();
-  }, [animate, scope]);
+    const interval = setInterval(() => {
+      setCurrentIndex((prev) => (prev + 2) % puns.length);
+    }, SLIDE_INTERVAL);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div
+      className="relative w-72 border-2 border-red-400"
       style={{
-        clipPath: "polygon(0 0, 100% 0, 100% 40px, 0 40px)",
-        overflow: "hidden",
+        height: height * 2 || "auto",
       }}
     >
-      <motion.p ref={scope} className="text-sm uppercase tracking-widest">
-        {[...puns, ...puns, ...puns].map((pun, index) => (
-          <span key={index} className="block">
-            {pun}
-          </span>
-        ))}
-      </motion.p>
+      <motion.div
+        animate={{ y: -currentIndex * height }}
+        transition={{
+          duration: SLIDE_INTERVAL / 1000 - 1,
+          ease: "easeInOut",
+          delay: 0,
+        }}
+        className="absolute w-72 whitespace-nowrap border-x-2 border-emerald-400 text-sm"
+      >
+        <span
+          ref={measureRef}
+          className="absolute right-0 top-0 block"
+          aria-hidden="true"
+        >
+          |
+        </span>
+        {[...puns, ...puns.slice(0, 2)].map((pun, index) => {
+          return <div key={index}>{pun}</div>;
+        })}
+      </motion.div>
     </div>
   );
 }
