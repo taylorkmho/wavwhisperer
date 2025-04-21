76fcf614817cb37c6062e5a1c24f89515c5d6862
Taylor Ho <taylorkmho@gmail.com>
2025-04-20 23:47:05 -0700

    balanced steady

--- components/SurfReport/BottomNav/WaveHeights.tsx
+++ components/SurfReport/BottomNav/WaveHeights.tsx
@@ -1,5 +1,9 @@
 import { cn } from "@/lib/utils";
-import { FaArrowTrendDown, FaArrowTrendUp } from "react-icons/fa6";
+import {
+  FaArrowRightLong,
+  FaArrowTrendDown,
+  FaArrowTrendUp,
+} from "react-icons/fa6";
 
 type WaveHeight = {
   direction: string;
@@ -44,13 +48,17 @@ export function WaveHeights({
               variant === "display" && "text-xl leading-none md:text-xl"
             )}
           >
-            {wave.height} {variant !== "display" && " ft"}
+            {wave.height}
+            {variant === "display" ? <>&rsquo;</> : " ft"}
           </span>
           <span
             className={cn(
               "flex h-5 items-center gap-1.5 rounded-full px-2 text-xs",
               wave.trend === "increasing" && "bg-green-500/10 text-green-500",
-              wave.trend === "decreasing" && "bg-red-500/10 text-red-500"
+              wave.trend === "decreasing" && "bg-red-500/10 text-red-500",
+              variant === "default" &&
+                wave.trend === "steady" &&
+                "bg-muted-foreground/10 text-muted-foreground"
             )}
             title={
               wave.trend === "increasing"
@@ -61,11 +69,13 @@ export function WaveHeights({
             }
           >
             {wave.trend &&
-              (wave.trend === "increasing" ? (
-                <FaArrowTrendUp className="size-3" />
-              ) : wave.trend === "decreasing" ? (
-                <FaArrowTrendDown className="size-3" />
-              ) : null)}
+              {
+                increasing: <FaArrowTrendUp className="size-3" />,
+                decreasing: <FaArrowTrendDown className="size-3" />,
+                steady: variant === "default" && (
+                  <FaArrowRightLong className="size-3" />
+                ),
+              }[wave.trend]}
           </span>
         </div>
       ))}
